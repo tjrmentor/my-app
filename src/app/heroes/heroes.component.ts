e23222d693eec8df1ee7f8e81ebe1824cd44b6dc
Taylor Richard <tjrmentor@gmail.com>
2017-12-05 15:26:29 -0500

    Added Master/Display template and logic

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,10 +11,11 @@ export class HeroesComponent implements OnInit {
   heroes = HEROES;
 
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-   };
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
   constructor () {
 
