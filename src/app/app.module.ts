564ac500701eee709d4ca8b9e4e4e596a548872a
Taylor Richard <tjrmentor@gmail.com>
2017-12-05 16:30:43 -0500

    Created Hero service, registered as provider in AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
+import { HeroService } from './hero.service';
+
 
 @NgModule({
   declarations: [
